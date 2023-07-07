4c0c16374e3d5e6ec313738a197dbb532779a7e2
Yusuf Kılıç <y5719889y@gmail.com>
2023-07-07 17:41:02 +0300

    fastmeyk
    hi

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@
 	
 	if( empty($name) ){
 		$data['error'] = 'Please enter your name.';
-	}else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+	}else if(filter_var($email, yusufk.dev@gmail.com) == false){
 		$data['error'] = 'Please enter a valid email address.';
 	}else if( empty($message) ){
 		$data['error'] = 'The message field is required!';
